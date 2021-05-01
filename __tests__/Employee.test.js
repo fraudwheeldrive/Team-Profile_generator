7ecc64450c72a2d51626cb64d201632415e39cf8
[Andrew Core] <[adcore89@gmail.com]>
2021-05-01 14:55:52 -0400

    employee test functionality completed, to refine for error catching

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,18 @@
-test ('creates employee based on inquierer input', () => {
-    const employee = new Employee();
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(String));
-    expect(employee.email).toEqual(expect.any(String));
-    expect(employee.email).toEqual(expect.any(String));
-});
 
-module.export = Employee
+const Employee = require('../lib/Employee');
+
+test('create a employee object', () => {
+const employee = new Employee('Andrew' , 'adcore89@gmail.com' , '9000' , '8675309' , 'cool dude');
+
+expect(employee.name).toBe("Andrew");
+expect(employee.email).toBe("adcore89@gmail.com");
+expect(employee.id).toBe("9000");
+expect(employee.officeNumber).toBe("8675309");
+expect(employee.role).toBe("cool dude");
+
+})
+
+
+
+module.export = Employee;
